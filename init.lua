8b21d20217818b8a312485806f7a598390651d4b
Rafael Lima <rafa.cbl@gmail.com>
2023-12-26 11:09:55 -0300

    make colorscheme use lazy setup

--- init.lua
+++ init.lua
@@ -1,7 +1,25 @@
-require("rafael.plugins-setup")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- set leader key to space
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins")
+
+vim.cmd('colorscheme rose-pine')
+
 require("rafael.core.options")
 require("rafael.core.keymaps")
-require("rafael.plugins.colorscheme")
 require("rafael.plugins.comment")
 require("rafael.plugins.nvim-tree")
 require("rafael.plugins.lualine")
