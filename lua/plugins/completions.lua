bfcf48e6b2deb56266db015e57ee4b40ab3a36c9
Rafael Lima <rafa.cbl@gmail.com>
2024-01-05 11:55:40 -0300

    add capabilities for snippets and lsp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 return {
   {
+    "hrs7th/cmp-nvim-lsp"
+  },
+  {
     "L3MON4D3/LuaSnip",
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,11 +20,14 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.solargraph.setup({})
+
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.solargraph.setup({ capabilities = capabilities })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
