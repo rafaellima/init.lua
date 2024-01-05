96317333cefba9a769538635fb25ec93842213f2
Rafael Lima <rafa.cbl@gmail.com>
2024-01-05 10:30:06 -0300

    remove rubocop from none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,8 +7,6 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.diagnostics.eslint,
       },
     })
