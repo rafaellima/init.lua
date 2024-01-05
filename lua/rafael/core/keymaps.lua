c4f0a1e01b347d9adc581ce0ce352791ff742b69
Rafael Lima <rafa.cbl@gmail.com>
2024-01-05 10:29:58 -0300

    remove telescope keymaps

--- lua/rafael/core/keymaps.lua
+++ lua/rafael/core/keymaps.lua
@@ -42,13 +42,6 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 keymap.set("n", "<leader>tc", ":NvimTreeCollapse<CR>") -- collapse file explorer
 
--- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-
 -- neogit
 keymap.set("n", "<leader>ns", ":Neogit<CR>") -- open neogit status
 
