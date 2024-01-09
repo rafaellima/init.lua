46ae8a29dc057409512878f30468b08f1315d1ed
Rafael Lima <rafa.cbl@gmail.com>
2024-01-09 11:24:17 -0300

    add neotree configuration

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,22 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      vim.keymap.set("n", "<leader>e", ":Neotree<CR>", {})
-    end,
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			local neotree = require("neotree")
+			neotree.setup({
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
+		end,
+	},
 }
