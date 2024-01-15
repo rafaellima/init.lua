0876382127c37c6b31adcdf87d50384a227bb751
Rafael Lima <rafa.cbl@gmail.com>
2024-01-15 12:05:32 -0300

    fix colorscheme setup (catpuccinn)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,22 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  config = function()
-    require("catppuccin").setup({
-      colorscheme = "frappe",
-      transparency = true,
-      integrations = {
-        harpoon = true,
-        neotree = true,
-      },
-    })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        integrations = {
+          harpoon = true,
+          neotree = true,
+          telescope = {
+            enabled = true,
+            style = "nvchad",
+          },
+        },
+      })
 
-    vim.cmd.colorscheme("catppuccin")
-  end,
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 }
