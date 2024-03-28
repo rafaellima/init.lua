f4720597a79335c8f466f31a379e6010414312a1
Rafael Lima <rafa.cbl@gmail.com>
2024-03-28 07:47:14 -0300

    add vim-test TestSuite keymap

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -3,6 +3,7 @@ return {
     "vim-test/vim-test",
     dependencies = { "preservim/vimux" },
     vim.keymap.set("n", "<leader>,t", ":TestNearest<CR>"),
+    vim.keymap.set("n", "<leader>,a", ":TestSuite<CR>"),
 
     vim.cmd("let test#strategy = 'vimux'")
   },
