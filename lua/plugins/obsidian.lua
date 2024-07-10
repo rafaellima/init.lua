7f767457ea5ceb83e06ad575f35f89ac25d92082
Rafael Lima <rafa.cbl@gmail.com>
2024-07-10 11:44:20 -0300

    feat: add obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,31 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "garden",
+        path = "~/Documents/The Garden",
+      },
+    },
+  },
+
+  notes_subdir = "0 - Inbox",
+  new_notes_location = "notes_subdir",
+
+  completion = {
+    nvim_cmp = true,
+    min_chars = 2,
+  },
+  templates = {
+    folder = "5 - Templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    substitutions = {},
+  },
+}
