c42dc9c797858e8de80df207e35c928d3ef8382a
Rafael Lima <rafa.cbl@gmail.com>
2024-11-19 09:52:56 -0300

    configure neotest output and support for golang and elixir

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -15,14 +15,18 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "olimorris/neotest-rspec",
       "nvim-neotest/neotest-go",
+      "jfpedroza/neotest-elixir",
     },
     vim.keymap.set("n", "<leader>,s", ":lua require('neotest').run.run()<CR>"),
     vim.keymap.set("n", "<leader>,f", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>"),
+    vim.keymap.set("n", "<leader>,o", ":lua require('neotest').output.open()<CR>"),
 
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-rspec")
+          require("neotest-rspec"),
+          require("neotest-elixir"),
+          require("neotest-go"),
         },
       })
     end
