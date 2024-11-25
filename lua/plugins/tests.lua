189f521706775985ac232aa84811f0a624e2d141
Rafael Lima <rafa.cbl@gmail.com>
2024-11-25 11:09:50 +0100

    remove neotest and keep vim-test

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -2,33 +2,10 @@ return {
   {
     "vim-test/vim-test",
     dependencies = { "preservim/vimux" },
-    vim.keymap.set("n", "<leader>,t", ":TestNearest<CR>"),
+    vim.keymap.set("n", "<leader>,s", ":TestNearest<CR>"),
+    vim.keymap.set("n", "<leader>,f", ":TestFile<CR>"),
     vim.keymap.set("n", "<leader>,a", ":TestSuite<CR>"),
 
     vim.cmd("let test#strategy = 'vimux'")
   },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "olimorris/neotest-rspec",
-      "nvim-neotest/neotest-go",
-      "jfpedroza/neotest-elixir",
-    },
-    vim.keymap.set("n", "<leader>,s", ":lua require('neotest').run.run()<CR>"),
-    vim.keymap.set("n", "<leader>,f", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>"),
-    vim.keymap.set("n", "<leader>,o", ":lua require('neotest').output.open()<CR>"),
-
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-rspec"),
-          require("neotest-elixir"),
-          require("neotest-go"),
-        },
-      })
-    end
-  }
 }
