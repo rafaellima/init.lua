c21310103bfccf4e621a18aa5b99f4def8de66af
Rafael Lima <rafa.cbl@gmail.com>
2024-12-05 13:10:12 -0300

    upgrade telescope to 0.1.8

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
   {
-    "nvim-telescope/telescope.nvim", tag = "0.1.5",
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup {
         pickers = {
           find_files = {
             theme = "dropdown",
+            previewer = false,
           },
           live_grep = {
             theme = "dropdown",
