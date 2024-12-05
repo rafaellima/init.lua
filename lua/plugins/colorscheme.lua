69fdf571aa6d60662acda90d7b34a9ea8c111e9b
Rafael Lima <rafa.cbl@gmail.com>
2024-12-05 13:08:59 -0300

    replace colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,25 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
+    opts = {},
+
     config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        -- color_overrides = {
-        --   mocha = {
-        --     base = "#000000",
-        --     mantle = "#000000",
-        --     crust = "#000000",
-        --   },
-        -- },
-        integrations = {
-          harpoon = true,
-          neotree = true,
-          telescope = {
-            enabled = true,
-            style = "nvchad",
-          },
+      require("tokyonight").setup({
+        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
         },
       })
 
-      vim.cmd.colorscheme("catppuccin-mocha")
+      vim.cmd [[colorscheme tokyonight]]
     end,
-  },
+  }
 }
