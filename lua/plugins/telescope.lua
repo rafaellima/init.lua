a61f1b9a88106a69b2cd04651a1e3f347ef519a2
Rafael Lima <642494+rafaellima@users.noreply.github.com>
2024-12-12 09:30:01 -0300

    set keymap for telescope builtin.git_files function

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,32 @@
 return {
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						theme = "dropdown",
-						previewer = false,
-					},
-					-- live_grep = {
-					-- 	theme = "dropdown",
-					-- },
-				},
-				extensions = { fzf = {} },
-			})
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+            previewer = false,
+          },
+          -- live_grep = {
+          -- 	theme = "dropdown",
+          -- },
+        },
+        extensions = { fzf = {} },
+      })
 
-			require("telescope").load_extension("fzf")
+      require("telescope").load_extension("fzf")
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fg", require("plugins.custom.telescope.multigrep"))
-		end,
-	},
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>fp", builtin.find_files, {})
+      -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>fg", require("plugins.custom.telescope.multigrep"))
+    end,
+  },
 }
