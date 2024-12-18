d2dac9d47496e4f7113608f07eb775ff3c3c271f
Rafael Lima <642494+rafaellima@users.noreply.github.com>
2024-12-18 13:27:49 -0300

    replace completions with blink.cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,41 +1,18 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    config = function()
-      local cmp = require'cmp'
-      require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'supermaven' },
-          { name = 'luasnip' }, -- For luasnip users.
-        }, {
-            { name = 'buffer' },
-          })
-      })
-    end,
-  }
+		version = "v0.*",
+		opts = {
+			keymap = { preset = "default" },
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+
+			signature = { enabled = true },
+		},
+	},
 }
