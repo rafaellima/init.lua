374a9748246f4c715ed7ce0ce13ce0385c395154
Rafael Lima <642494+rafaellima@users.noreply.github.com>
2025-08-27 10:33:25 -0300

    add light colorscheme tokyonight-day

--- lua/rafael/core/options.lua
+++ lua/rafael/core/options.lua
@@ -1,24 +1,24 @@
-local opt = vim.opt -- for conciseness
+local opt = vim.opt   -- for conciseness
 
 opt.timeoutlen = 3000 -- time to wait for a mapped sequence to complete (in milliseconds)
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
-opt.scrolloff = 8 -- keep the cursor 8 lines from the top/bottom of the screen
+opt.scrolloff = 8     -- keep the cursor 8 lines from the top/bottom of the screen
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -28,7 +28,7 @@ opt.cursorline = true -- highlight the current cursor line
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+-- opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
@@ -38,11 +38,11 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true     -- split vertical window to the right
+opt.splitbelow = true     -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
-opt.maxmempattern=5000 -- the maximum amount of memory that can be used for pattern matching operations.
+opt.maxmempattern = 5000  -- the maximum amount of memory that can be used for pattern matching operations.
 
 --  obsidian.nvim ui
 opt.conceallevel = 1
