5f62cd1a02e36f72607e9c8f5e08f7b607a3f7b3
Rafael Lima <642494+rafaellima@users.noreply.github.com>
2025-12-01 14:20:55 -0300

    set catppuccin background opacity

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,6 +6,7 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "macchiato",
+        transparent_background = true,
       })
 
       vim.cmd("colorscheme catppuccin")
